cae8bdaa2a33cd917c5cb761850d7904afe021d8
Fernando B <fredz0003@gmail.com>
2019-09-29 17:34:39 -0500

    Updates

---
+++ precache-manifest.d9cb0e48f54b92e62f695de851e49b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da86136e2ef41af4bef9da2b41eb7801",
+    "url": "/family-trivia-react/index.html"
+  },
+  {
+    "revision": "80e9d2527d55bbbce497",
+    "url": "/family-trivia-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5197b992aa60c93ea42a",
+    "url": "/family-trivia-react/static/js/2.ff9b2c9a.chunk.js"
+  },
+  {
+    "revision": "80e9d2527d55bbbce497",
+    "url": "/family-trivia-react/static/js/main.e04eade6.chunk.js"
+  },
+  {
+    "revision": "c9947c6f581c88eb0e5f",
+    "url": "/family-trivia-react/static/js/runtime-main.e9f70d45.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/family-trivia-react/static/media/logo.25bf045c.svg"
+  }
+]);
